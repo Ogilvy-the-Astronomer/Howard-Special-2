4d2eb3cf760205f1fbe26c9fe1b9c82330e3677d
Howard van Waard <thethirdnexusofalltime@gmail.com>
2019-11-11 14:54:57 +0000

    minor changes

--- src/HowardSpecial2/Camera.cpp
+++ src/HowardSpecial2/Camera.cpp
@@ -7,13 +7,11 @@ Camera::Camera(){
 	projection = glm::perspective(glm::radians(90.0f), (float)GetCore()->window_w / (float)GetCore()->window_h, 0.1f, 1000.f);
 }
 
-glm::mat4 Camera::GetView()
-{
+glm::mat4 Camera::GetView(){
 	return glm::inverse(GetGameObject()->GetComponent<Transform>()->GetModel());
 }
 
-glm::mat4 Camera::GetProjection()
-{
+glm::mat4 Camera::GetProjection(){
 
 	return projection;
 }
